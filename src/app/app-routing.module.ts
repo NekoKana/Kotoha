65ad7a754ea779eb996374327f4b40c882ee1838
towa0131 <towa.towa0131@gmail.com>
2021-05-05 02:55:34 +0900

    [Add] アカウント登録を実装
    - サインアップを実装
    
    - ログインページとcssを共通化
    
    - ログインページの不具合を修正

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,11 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+      loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
   }
 ];
 @NgModule({
