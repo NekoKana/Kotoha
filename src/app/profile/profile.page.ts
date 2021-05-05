91bb713db11e39c5aa6c86314b128bc33fc58400
Neko419 <sabutalong1121@gmail.com>
2021-05-05 10:32:22 +0900

    [Update] 未ログイン時にログイン画面へ強制遷移

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -13,6 +14,14 @@ export class ProfilePage {
   last_name: string = localStorage.last_name;
   birth: string = localStorage.birth;
 
-  constructor() {}
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    if (this.email == null) {
+      this.router.navigate(['login']);
+    }
+  }
   
 }
