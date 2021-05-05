4976f7aaaeea7634230f3071d794da3c28aa554f
Neko419 <sabutalong1121@gmail.com>
2021-05-05 14:12:05 +0900

    [Add] 家族IDの表示を追加

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -10,6 +10,7 @@ export class ProfilePage {
   email: string;
   user_id: string;
   user_name: string;
+  family_id: string;
   name : string;
   birth: string;
 
@@ -24,6 +25,7 @@ export class ProfilePage {
       this.email = localStorage.email;
       this.user_id = localStorage.user_id;
       this.user_name = localStorage.user_name;
+      this.family_id = localStorage.family_id;
       this.name = localStorage.last_name + ' ' + localStorage.first_name;
       this.birth = localStorage.birth;
     }
