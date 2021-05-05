871442ff1420d753f917b90851d0e8ac002042c2
Neko419 <sabutalong1121@gmail.com>
2021-05-05 13:14:55 +0900

    [Change]日付の持ってくる場所を変更

--- src/app/calendar/calendar.page.ts
+++ src/app/calendar/calendar.page.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { CalendarMode } from 'ionic2-calendar/calendar';
-
-
 
 @Component({
   selector: 'app-home',
@@ -29,15 +28,14 @@ export class CalendarPage {
   onEventSelected(event) {
     console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
   }
-
   onTimeSelected(ev) {
-    console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
-      (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+    // console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
+    //  (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
     this.selectedDate = ev.selectedTime;
   }
 
   onCurrentDateChanged(event: Date) {
-    console.log('current date change: ' + event);
+    // console.log('current date change: ' + event);
   }
 
   onRangeChanged(ev) {
