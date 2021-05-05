7ab191f42a8e853c8eab56016631f4ecc37cd966
Hayabusa1601 <1601hayabusa@gmail.com>
2021-05-05 11:41:46 +0900

    [Update] サーバとの通信処理の完成

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,11 +6,13 @@ import { AlertController } from '@ionic/angular';
 import { SharePost } from './tab1.model';
 import { tick } from '@angular/core/testing';
 import { EmailValidator } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page implements OnInit {
   /*通信テスト用変数*/ 
   content_id: number = 1;
@@ -31,20 +33,31 @@ export class Tab1Page implements OnInit {
   
   getContent: SharePost[];
   
-  ngOnInit () {
-    localStorage.family_id = 'xxxxx';
-  }
-
-
-
-  
   /*コンストラクタ*/
   constructor(
+    /*gsたち */
     public gs: GlobalService,
     public gs2: GlobalService,
     public gs3: GlobalService,
-    private alertController: AlertController
+    private alertController: AlertController,
+
+    /*emailがnullだった場合、画面遷移させる準備 */
+   /* private router: Router*/
+
   ) {}
+
+    
+  ngOnInit () {
+    localStorage.family_id = 'tylgophxsxpqrmhbbyxe';
+    this.get();
+
+    /*
+     email == nullだった場合、画面遷移させる
+    if(this.email == null) {
+      this.router.navigate(['login']);
+    }
+    */
+  }
   
   /*シェアボードに乗せるリスト(テスト用も兼ねてる) */
  
@@ -93,6 +106,7 @@ export class Tab1Page implements OnInit {
     this.delete(share);
   }
 
+  /*シェア追加の処理 */
   addSharePost(share: SharePost): void {
     this.shares.push(share);
   }
@@ -101,10 +115,11 @@ export class Tab1Page implements OnInit {
   async onAddButtonClicked(): Promise<void> {
     const alert = await this.alertController.create({
 
-      //メッセージ
+      //メッセーじ
       header: '追加',
       subHeader: '',
       message: '家族に伝えたいことは何？',
+      
 
       //ボタン　CancelとOKがあり選択によって処理が変わる
       buttons: [{
@@ -119,15 +134,23 @@ export class Tab1Page implements OnInit {
         text: 'OK',
         handler: (data) => {
 
-          //alertを用いたshare変数への代入
+          //alertを用いた変数shareへの代入
           const share = new SharePost();
-          share.content_id = Number((new Date).getTime().toString());
-          console.log(share.content_id);
+
+          
+          //日付(期日)
+          share.due = new Date(data.due);
+          console.log(share.due);
+
+          //本文
           share.content = data.share;
           console.log(share.content);
 
+          //未完了のフラグを建てる
+          share.is_completed = false;
+
           //put通信処理
-          this.register(share)           
+          share.content_id = this.register(share);           
 
           //ShareBoardへの追加処理
           this.addSharePost(share);  
@@ -140,8 +163,14 @@ export class Tab1Page implements OnInit {
       inputs: [{
         name: 'share',
         id:'new-share',
-        placeholder: '家族に伝えなきゃいけないことは何？'
-      }]
+        placeholder: '本文'
+      },
+      {
+        name: 'due',
+        id: 'new-due',
+        type: 'date',
+      }
+    ]
     });
 
     await alert.present();
@@ -152,33 +181,81 @@ export class Tab1Page implements OnInit {
 
 
   /*Put通信処理 */
-  register = (share: SharePost) => {
+  register(share: SharePost): number {
+    
+    //shareをサーバに送信
     this.registerPostObj['family_id'] = localStorage.family_id;
     this.registerPostObj['content'] = share.content;
+    this.registerPostObj['date'] = this.getDueString(share);
+
     const registbody = this.registerPostObj;
 
+
     this.gs.http('/shareboard/put.php', registbody).subscribe(
+
       res => {
+
         this.registerReturnObj = res;
+
         this.putResult = this.registerReturnObj['result'];
-        if(this.registerReturnObj['result']) {
+        console.log(this.registerReturnObj['result']);
+
+        if(this.registerReturnObj['result'] == true) {
           console.log('Communication passed!!');
         }else{
           console.log('Communication failed......');
         }
+
+  
       }
+
     )
+    //content_idを返す
+    return this.registerReturnObj['content_id'];
+    console.log(this.registerReturnObj['content_id']);
+
+
   }
 
 
   /*get通信処理 */
   get = () => {
-    this.getPostObj['family_id'] = "tylgophxsxpqrmhbbyxe";
+    this.getPostObj['family_id'] = localStorage.family_id;
+
     const getbody = this.getPostObj;
     
     this.gs2.http('/shareboard/get.php', getbody).subscribe(
       res => {
-        this.getReturnObj = res;
+        this.getReturnObj = res;//レスポンスをgetReturnObjに代
+
+        this.getReturnObj['contents'].forEach(element => {
+          
+          //get：受け取り処理
+
+            const share = new SharePost();
+
+            //content_idの受け取り
+            share.content_id = element['content_id'];
+            console.log(share.content_id);
+          
+            //日付情報の取得
+            share.due = element['date'];
+            console.log(share.due);
+            
+            //本文の受け取り
+            share.content = element['content'];
+            console.log(share.content);
+
+            //完了済みかの受け取り
+            share.is_completed = element['is_completed'];
+            console.log(share.is_completed);
+  
+            //ShareBoardへの追加処理
+            this.addSharePost(share);  
+  
+  
+          
+        });
         console.log(this.getReturnObj);
         // this.shares = this.getReturnObj;
       }
@@ -231,5 +308,13 @@ export class Tab1Page implements OnInit {
         
       }
     )
+  }
+
+  getDueString(share: SharePost): string {
+    const year: string = share.due.getFullYear().toString();
+    const month: string = (share.due.getMonth() + 1).toString();
+    const date: string = share.due.getDate().toString();
+
+    return year + '-' + month + '-' + date;
   }
 }
